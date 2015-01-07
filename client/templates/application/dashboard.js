7a1464eba75b15cff2a90c0274aa137cb8bb5ed6
Ben Venker <bvemails@gmail.com>
2015-01-07 16:38:54 -0600

    Revert 7db70aa..5e4cc80
    This rolls back to commit 7db70aa0dfed0e6f91707cd87171ab29cfecfb32.

--- client/templates/application/dashboard.js
+++ client/templates/application/dashboard.js
@@ -2,6 +2,5 @@ Template.dashboard.events({
   'click .logout': function(event){
     event.preventDefault();
     Meteor.logout();
-    Router.go('landingPage');
   }
 });
--- client/templates/application/register.js
+++ client/templates/application/register.js
@@ -3,13 +3,10 @@ Template.register.events({
     event.preventDefault();
     var emailVar = template.find('#register-email').value;
     var passwordVar = template.find('#register-password').value;
-    var users = [];
-    var userid = Accounts.createUser({
-          email: emailVar,
-          password: passwordVar,
+    Accounts.createUser({
+      email: emailVar,
+      password: passwordVar
     });
-    Meteor.users.update({_id: userid}, {$set: {'emails.0.verified': true}});
-    Roles.addUsersToRoles(userid, ['view-invoices','create-invoices']);
     console.log("Form submitted.");
   }
 });
