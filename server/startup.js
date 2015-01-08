a3c52e11ce98621febb5292e3843ba05bf79c17d
Ben Venker <bvemails@gmail.com>
2015-01-08 17:44:55 -0600

    First draft of CIU export.
    This is a nightmare and badly needs to be refactored!

---
+++ server/startup.js
@@ -0,0 +1,13 @@
+// Meteor.startup(function() {
+//   fs = Npm.require('fs');
+//
+//   fs.writeFile('../server/ciu.txt', 'Hello CIU', function(err) {
+//     if (err) throw err;
+//     console.log('It\s saved');
+//   });
+//
+//   fs.writeFile('fixtures.js', function(err) {
+//     if (err) throw err;
+//     console.log(fixtures.js);
+//   })
+// })
