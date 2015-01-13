a4b801ed9bef722609d5845c2f34d92f133e7815
Ben Venker <bvemails@gmail.com>
2015-01-12 22:30:54 -0600

    Added find-faster and updated find queries for mfg.'s.
    Query time dropped quite a bit. Definitely loading almost 2x as fast on
    initial page invoice_submit_form.

--- client/templates/invoice_submit_form/manufacturers.js
+++ client/templates/invoice_submit_form/manufacturers.js
@@ -8,7 +8,7 @@ Template.manufacturers.helpers({
   manufacturers: function() {
     // TODO: add manufacturers collection
 
-      var everything = Manufacturers.find({department: Session.get('department')},{sort: {manufacturer:1}}).fetch();
+      var everything = Manufacturers.findFaster({department: Session.get('department')},{sort: {manufacturer:1}}).fetch();
       var justManufacturers = _.pluck(everything,"manufacturer");
       return _.uniq(justManufacturers);
     }
