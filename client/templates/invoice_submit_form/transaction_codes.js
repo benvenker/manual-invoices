a8b1269be203fdd99bdb466c0e8a1b3f7f877367
Ben Venker <bvemails@gmail.com>
2015-01-23 12:29:37 -0600

    Restructured project directories.

--- client/templates/invoice_submit_form/transaction_codes.js
+++ client/templates/invoice_submit_form/transaction_codes.js
@@ -6,14 +6,16 @@ Template.transactionCodes.created = function() {
 
 Template.transactionCodes.helpers({
   transactionCodes: function() {
-    var everything = TransactionCodes.find({banner: parseInt(Session.get('opco'))}, {sort: {transactionCode:1}}).fetch();
-    var justTransactionCodes = _.pluck(everything,"transactionCode");
-    return _.uniq(justTransactionCodes);
+    //var everything = TransactionCodes.find({banner: parseInt(Session.get('opco'))}, {sort: {transactionCode:1}}).fetch();
+    //var justTransactionCodes = _.pluck(everything,"transactionCode");
+    //return _.uniq(justTransactionCodes);
+
+    return TransactionCodes.findFaster({banner: parseInt(Session.get('opco'))}, {sort: {transactionCode:1}}).fetch();
   }
 });
 
 Template.transactionCodes.events({
   "change .transactionCode-selection": function(e, t){
-    return Session.set("transactionCode", $("[name=transactionCodes]").val());
+    return Session.set("transactionCode", $("[name=transactionCodes]").val().substr(0,3));
   }
 });
