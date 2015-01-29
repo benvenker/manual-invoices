cfcac6e2141fca8902a298406f80fc77c88965a0
Ben Venker <bvemails@gmail.com>
2015-01-28 18:10:27 -0600

    Add retail cost to header and line templates.

--- client/templates/invoice_submit_form/invoice_submit_form.js
+++ client/templates/invoice_submit_form/invoice_submit_form.js
@@ -1,7 +1,7 @@
 Template.invoiceSubmitForm.events({
   'click .add-invoice-line': function(e) {
     e.preventDefault();
-      var newRow = $('<tr><td><input type="text" class="store" maxlength=4></td><td><input type="text" class="class"></td><td><input type="text" class="unitCost" maxlength=11></td><td><input type="text" class="quantity" value=""></td><td><input type="text" class="style" maxlength=20 value=""></td><td><input type="text" class="sku" value="" maxlength=8></td><td><input type="text" class="description" value="" maxlength=200></td><td><a class="remove-invoice-line button-red tiny"><b>×</b></a></td></tr>');
+      var newRow = $('<tr><td><input type="text" class="store" maxlength=4></td><td><input type="text" class="class"></td><td><input type="text" class="unitCost" maxlength=11></td><td><input type="text" class="lineRetailCost" maxlength=11></td><td><input type="text" class="quantity" value=""></td><td><input type="text" class="style" maxlength=20 value=""></td><td><input type="text" class="sku" value="" maxlength=8></td><td><input type="text" class="description" value="" maxlength=200></td><td><a class="remove-invoice-line button-red tiny"><b>×</b></a></td></tr>');
 
       $('table.flakes-table').append(newRow);
   },
@@ -15,7 +15,7 @@ Template.invoiceSubmitForm.events({
     console.log("GL Account: " + glAccount);
     var invoiceAmount = 0;
     var totalQuantity = 0;
-    var retailCost = form.find('[name=retailCost]').val();
+    var retailCost = 0;
 
 
     // Get the header values
@@ -37,7 +37,6 @@ Template.invoiceSubmitForm.events({
       headerDescription: form.find('[name=headerDescription]').val(),
       urn: form.find('[name=urn]').val(),
       submitted: moment(new Date()).format('L'),
-      retailCost: numeral().unformat(retailCost),
       glAccount: glAccount[0], // Since underscore returns an array, get the
                                // first element, which contains the GL account
     }
@@ -60,7 +59,9 @@ Template.invoiceSubmitForm.events({
 
       var $tds = $(this).find('td input');
       var unitCost = parseFloat(($tds.eq(2).val()));
-      var quantity = parseInt($tds.eq(3).val());
+      var lineRetailCost = parseFloat(($tds.eq(3).val()));
+      var quantity = parseInt($tds.eq(4).val());
+
       var invoiceLine = {
         invoiceId: invoice._id,
         invoiceNumber: invoice.invoiceNumber,
@@ -68,16 +69,20 @@ Template.invoiceSubmitForm.events({
         store: parseInt($tds.eq(0).val()),
         itemClass: $tds.eq(1).val(),
         unitCost: numeral(unitCost).format('00.00'),
+        lineRetailCost: numeral(lineRetailCost).format('00.00'),
         quantity: quantity,
-        style: $tds.eq(4).val(),
-        sku: $tds.eq(5).val(),
-        description: $tds.eq(6).val(),
+        style: $tds.eq(5).val(),
+        sku: $tds.eq(6).val(),
+        description: $tds.eq(7).val(),
         lineTotal: numeral(unitCost * quantity).format('00.00'),
         submitted: moment(new Date()).format('L'),
       }
 
       var lineTotalVar = numeral(unitCost * quantity).format('00.00');
+      var lineRetailCostVar = numeral(lineRetailCost * quantity).format('00.00');
+
       invoiceAmount += numeral().unformat(lineTotalVar);
+      retailCost += numeral().unformat(lineRetailCostVar);
       totalQuantity += quantity;
       InvoiceLines.insert(invoiceLine);
 
@@ -88,7 +93,8 @@ Template.invoiceSubmitForm.events({
       //
       var invoiceProperties = {
         totalCost: numeral(invoiceAmount).format('00.00'),
-        totalQuantity: totalQuantity,
+        retailCost: numeral(retailCost).format('00.00'),
+        totalQuantity: totalQuantity
         // vendorNumber: vendorNumber
       };
       //console.log(invoiceLineNum);
