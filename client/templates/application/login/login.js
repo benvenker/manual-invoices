383f6602108e9d87bd61bb27b3ace9865ceb61bc
Ben Venker <bvemails@gmail.com>
2015-02-17 16:29:07 -0600

    Added data validation to login form.

--- client/templates/application/login/login.js
+++ client/templates/application/login/login.js
@@ -1,14 +1,18 @@
 Template.login.events({
   'submit form': function(event, template){
     event.preventDefault();
-    var emailVar = template.find('#login-email').value;
-    var passwordVar = template.find('#login-password').value;
-    Meteor.loginWithPassword(emailVar, passwordVar, function(err) {
-      if (err) {
-        alert(err.reason);
-      } else
-          return false;
-    });
+    var email = trimInput(template.find('#login-email').value);
+    var password = template.find('#login-password').value;
 
+    if (isNotEmpty(email) && isEmail(email) && isNotEmpty(password) && isValidPassword(password)) {
+
+      Meteor.loginWithPassword(email, password, function (err) {
+        if (err) {
+          alert(err.reason);
+        } else
+        Router.go('invoiceSubmitForm');
+          return false;
+      });
+    }
   }
 });
