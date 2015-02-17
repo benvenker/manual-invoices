f2c26e53fc1dabb38e11061f74f23dea8d9e1471
Ben Venker <bvemails@gmail.com>
2015-02-17 16:43:08 -0600

    Added confirm password functionality to registration.

--- client/templates/application/login/register.js
+++ client/templates/application/login/register.js
@@ -1,15 +1,24 @@
 Template.register.events({
-  'submit form': function(event, template){
+  'submit form': function(event, template) {
     event.preventDefault();
-    var emailVar = template.find('#register-email').value;
-    var passwordVar = template.find('#register-password').value;
-    Accounts.createUser({email: emailVar, password: passwordVar}, function(err) {
-      if (err) {
-        alert(err.reason)
-      } else {
-        return true;
-      }
-    });
-    console.log("Form submitted.");
+    var email = template.find('#register-email').value;
+    var password = template.find('#register-password').value;
+    var passwordConfirm = template.find('#login-password-confirm').value;
+
+    if (isNotEmpty(email) && isNotEmpty(password) && isEmail(email) && areValidPasswords(password, passwordConfirm)) {
+
+      Accounts.createUser({email: email, password: password}, function (err) {
+        if (err) {
+          if (err.message === 'Email already exists. [403]') {
+            alert('We are sorry but this email is already used.');
+          } else {
+            alert('We are sorry but something went wrong.');
+          }
+        } else
+          alert('Congrats new Meteorite, you\'re in!');
+        Router.go('invoiceSubmitForm');
+        return false;
+      });
+    }
   }
 });
