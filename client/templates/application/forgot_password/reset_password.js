df2d31f47c16ee40b2b980985bd954e8abd9f6c7
Ben Venker <bvemails@gmail.com>
2015-02-17 16:26:53 -0600

    Added reset password template and helpers.

---
+++ client/templates/application/forgot_password/reset_password.js
@@ -0,0 +1,32 @@
+Template.resetPassword.events({
+  'submit #resetPasswordForm': function(e, t) {
+    e.preventDefault();
+
+    var resetPasswordForm = $(e.currentTarget),
+        password = resetPasswordForm.find('#resetPasswordPassword').val(),
+        passwordConfirm = resetPasswordForm.find('#resetPasswordPasswordConfirm').val();
+        email = trimInput(resetPasswordForm.find('#resetPasswordEmail').val().toLowerCase());
+
+    if (isNotEmpty(email) && isEmail(email) && isNotEmpty(password) && areValidPasswords(password, passwordConfirm)) {
+      Accounts.resetPassword({email: email}, password, function(err) {
+        if (err) {
+          if (err.message === 'User not found [403]') {
+            console.log('This email does not exist.');
+          } else {
+            console.log('We are sorry but something went wrong.');
+          }
+        } else {
+          alert('Your password has been changed. Welcome back!');
+          Router.go('landingPage')
+        }
+      });
+    }
+    return false;
+  }
+});
+
+Template.resetPassword.helpers({
+  resetPassword: function(){
+    return Session.get('resetPassword');
+  }
+});
