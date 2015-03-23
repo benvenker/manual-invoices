b2dccad7508b4bbc0c25c160d2f98a394f0d84ab
Ben Venker <bvemails@gmail.com>
2015-03-23 16:44:58 -0500

    Only show GL account template if required.
    Not all GL accounts need to be manually entered. Created a UI helper to
    only show the GL template if the GL account can’t be automatically
    assigned.

---
+++ client/helpers/global_helpers.js
@@ -0,0 +1,30 @@
+// If GL account needs to be manually entered, show the GL account template.
+// see /Users/Ben/Nitrous/manual-invoices/client/templates/invoice_submit_form/invoice_submit_form.html,
+// line 118: {{#if isGlTemplateRequired}}...{{/if}}
+UI.registerHelper('isGlTemplateRequired', function() {
+  if(Session.get('glRequired') == true) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+
+// Watch transactionCode session variable.
+Tracker.autorun(function() {
+  console.log("transCode = " + Session.get('transactionCode'));
+  var transCode = Session.get('transactionCode');
+  if (transCode == '160' ||
+      transCode == '210' ||
+      transCode == '211' ||
+      transCode == '212' ||
+      transCode == '327' ||
+      transCode == '500' ||
+      transCode == '501') {
+
+    Session.set('glRequired', true)
+  } else {
+    Session.set('glRequired', false);
+  }
+
+});
