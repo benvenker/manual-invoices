c5e692b3f6f1bdefcd389c62721232d034710db8
Ben Venker <bvemails@gmail.com>
2015-03-24 11:36:47 -0500

    Add Mongol package.

---
+++ client/helpers/mongol.js
@@ -0,0 +1,6 @@
+Session.set("Mongol", {
+  'collections': ['invoices', 'transactionCodes', 'manufacturers', 'invoiceLines', 'roles'],
+  'display': true,
+  'opacity_normal': ".7",
+  'opacity_expand': ".9"
+});
