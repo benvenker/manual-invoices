8e35ed889a1d08ceb2fb49c0a4068e25b2ef3bd1
Ben Venker <bvemails@gmail.com>
2015-03-26 18:30:11 -0500

    Fix template helper for archived invoices.

--- client/templates/archived_invoices/archived_invoices.js
+++ client/templates/archived_invoices/archived_invoices.js
@@ -1,5 +1,5 @@
 Template.archivedInvoices.helpers({
   invoices: function(){
-    return Invoices.find({$or: [{archived: true}, {exported: true}]}, {sort: {submitted:-1}});
+    return Invoices.find({$or: [{archived: true}, {exported: true}, {approved: true}]}, {sort: {submitted:-1}});
   }
 });
